95dc2525c6590acff402f310228e9fe600d2f3d3
Krish Ganatra <72177293+KrishJVG28@users.noreply.github.com>
2020-10-01 08:40:33 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void SieveOfEratosthenes(int n)
+void SieveOfEratosthenes(long long unsigned int Input)
 {
-
-    bool prime[n+1];
+    bool prime[Input+1];
     memset(prime, true, sizeof(prime));
 
-    for (int p=2; p*p<=n; p++)
+    for (int p=2; p*p<=Input; p++)
     {
-
         if (prime[p] == true)
         {
-
-            for (int i=p*2; i<=n; i += p)
+            for (int i=p*2; i<=Input; i += p)
                 prime[i] = false;
         }
     }
-
-
-    for (int p=2; p<=n; p++)
+    
+    for (int p=2; p<=Input; p++)
        if (prime[p])
           cout << p << " ";
 }
 
-
 int main()
 {
-    int n;
+    //int in which will the number that user enters will be stored
+    //long long unsigned int for bigger number capacity
+    long long unsigned int Input;
     cout << "Enter a number to check : " << endl;
-    cin >> n;
+    //User has to enter a number and it will be stored in Input
+    cin >> Input;
     cout << "Following are the prime numbers smaller "
-         << " than or equal to " << n << endl;
-    SieveOfEratosthenes(n);
+         << " than or equal to " << Input << endl;
+    SieveOfEratosthenes(Input);
     return 0;
 }
